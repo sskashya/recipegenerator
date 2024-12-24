a5ff0e9d9d08f9d4a7eff9675cd966253833b0c7
sskashya <156260545+sskashya@users.noreply.github.com>
2024-12-24 13:44:56 +0000

    fixed cookbot

--- cookbot.py
+++ cookbot.py
@@ -67,98 +67,100 @@ def recipe_search(prompt, number = 5, diet = None, exclude_ingredients = None, i
 
     if response.status_code == 200:
         recipes = response.json()
-        for recipe in recipes['results']:
-            st.header(f"Recipe: {recipe['title']}")
-            st.write(f"ID: {recipe['id']}")
-            st.image(f"{recipe['image']}")
-            if st.button:
-                expanded_recipe = st.expander("More Information")
-                expanded_recipe.write("-" * 40)
-                expanded_recipe.subheader(f"Servings: {recipe['servings']}")
-                for ingredient in recipe['missedIngredients']:
-                    expanded_recipe.write(f" - {ingredient['original']}")
-                expanded_recipe.write("-" * 40)
-                for instructions in recipe['analyzedInstructions']:
-                    if instructions['name'] == "":
-                        expanded_recipe.subheader(f"Prep\n ")
+        if recipes:
+            for recipe in recipes['results']:
+                st.header(f"Recipe: {recipe['title']}")
+                st.write(f"ID: {recipe['id']}")
+                st.image(f"{recipe['image']}")
+                if st.button:
+                    expanded_recipe = st.expander("More Information")
+                    expanded_recipe.write("-" * 40)
+                    expanded_recipe.subheader(f"Servings: {recipe['servings']}")
+                    for ingredient in recipe['missedIngredients']:
+                        expanded_recipe.write(f" - {ingredient['original']}")
+                    expanded_recipe.write("-" * 40)
+                    for instructions in recipe['analyzedInstructions']:
+                        if instructions['name'] == "":
+                            expanded_recipe.subheader(f"Prep\n ")
+                        else:
+                            expanded_recipe.subheader(f"{instructions['name']}\n ")
+                        for steps in instructions['steps']:
+                            expanded_recipe.write(f" Step: {steps['number']}\n - {steps['step']}\n ")
+                col1, col2 = st.columns(2, gap = 'medium')
+                with col1:
+                    save_recipe = st.button("Save", icon = '\U0001F4BE', key = recipe['id'])
+                with col2:
+                    shopping_cart = st.button("Add to Grocery List", icon = '\U0001F6D2', key = recipe['title'])
+                st.write("-" * 40)
+                if save_recipe:
+                    os.makedirs('file', exist_ok=True)
+                    log_file = f"file/recipe_hist_{st.session_state.username}.json"
+
+                    if os.path.exists(log_file):
+                        with open(log_file, 'r') as f:
+                            try:
+                                memories = json.load(f)
+                            except json.JSONDecodeError:
+                                memories = []
                     else:
-                        expanded_recipe.subheader(f"{instructions['name']}\n ")
-                    for steps in instructions['steps']:
-                        expanded_recipe.write(f" Step: {steps['number']}\n - {steps['step']}\n ")
-            col1, col2 = st.columns(2, gap = 'medium')
-            with col1:
-                save_recipe = st.button("Save", icon = '\U0001F4BE', key = recipe['id'])
-            with col2:
-                shopping_cart = st.button("Add to Grocery List", icon = '\U0001F6D2', key = recipe['title'])
-            st.write("-" * 40)
-            if save_recipe:
-                os.makedirs('file', exist_ok=True)
-                log_file = f"file/recipe_hist_{st.session_state.username}.json"
+                        memories = []
+                    
+                    recipe_hist = {
+                        "username": st.session_state.username,
+                        "date": datetime.now().date(),
+                        "recipe name": recipe['title'],
+                        "recipe id": recipe['id']
+                    }
 
-                if os.path.exists(log_file):
-                    with open(log_file, 'r') as f:
-                        try:
-                            memories = json.load(f)
-                        except json.JSONDecodeError:
-                            memories = []
-                else:
-                    memories = []
+                    memories = [
+                        memory for memory in memories
+                        if not (
+                            memory['recipe id'] == recipe_hist['recipe id'] and
+                            memory['date'] == recipe_hist['date']
+                        )
+                    ]
+                    memories.append(recipe_hist)
+
+                    with open(log_file, 'w') as f:
+                        json.dump(memories, f, indent=2)
+                    st.success("Recipe successfully saved!")
+
                 
-                recipe_hist = {
-                    "username": st.session_state.username,
-                    "date": datetime.now().date(),
-                    "recipe name": recipe['title'],
-                    "recipe id": recipe['id']
-                }
+                if shopping_cart:
+                    os.makedirs('grocery_file', exist_ok=True)
+                    log_file = f"grocery_file/prelim_shopping_list_{st.session_state.username}.json"
 
-                memories = [
-                    memory for memory in memories
-                    if not (
-                        memory['recipe id'] == recipe_hist['recipe id'] and
-                        memory['date'] == recipe_hist['date']
-                    )
-                ]
-                memories.append(recipe_hist)
+                    if os.path.exists(log_file):
+                        with open(log_file, 'r') as f:
+                            try:
+                                groceries = json.load(f)
+                            except json.JSONDecodeError:
+                                groceries = []
+                    else:
+                        groceries = []
+                    
+                    grocery_list = {
+                        "username": st.session_state.username,
+                        "date": datetime.now().date(),
+                        "recipe name": recipe['title'],
+                        "recipe id": recipe['id'],
+                        "ingredients": [ingredient['original'] for ingredient in recipe['missedIngredients']]
+                    }
 
-                with open(log_file, 'w') as f:
-                    json.dump(memories, f, indent=2)
-                st.success("Recipe successfully saved!")
+                    groceries = [
+                        grocery for grocery in groceries
+                        if not (
+                            grocery['recipe id'] == grocery_list['recipe id'] and
+                            grocery['recipe name'] == grocery_list['recipe name']
+                        )
+                    ]
+                    groceries.append(grocery_list)
 
-            
-            if shopping_cart:
-                os.makedirs('grocery_file', exist_ok=True)
-                log_file = f"grocery_file/prelim_shopping_list_{st.session_state.username}.json"
-
-                if os.path.exists(log_file):
-                    with open(log_file, 'r') as f:
-                        try:
-                            groceries = json.load(f)
-                        except json.JSONDecodeError:
-                            groceries = []
-                else:
-                    groceries = []
-                
-                grocery_list = {
-                    "username": st.session_state.username,
-                    "date": datetime.now().date(),
-                    "recipe name": recipe['title'],
-                    "recipe id": recipe['id'],
-                    "ingredients": [ingredient['original'] for ingredient in recipe['missedIngredients']]
-                }
-
-                groceries = [
-                    grocery for grocery in groceries
-                    if not (
-                        grocery['recipe id'] == grocery_list['recipe id'] and
-                        grocery['recipe name'] == grocery_list['recipe name']
-                    )
-                ]
-                groceries.append(grocery_list)
-
-                with open(log_file, 'w') as f:
-                    json.dump(memories, f, indent=2)
-                st.success("Ingredients added to Shopping List!")
-
+                    with open(log_file, 'w') as f:
+                        json.dump(memories, f, indent=2)
+                    st.success("Ingredients added to Shopping List!")
+        else:
+            st.write("No recipes are available for your search parameters")
     else:
         st.warning(f"Error: {response.status_code}")
         st.warning(response.text)
@@ -193,7 +195,7 @@ if 'username' in st.session_state:
 
     search = st.text_input("Search for meals here")
     if search:
-        diet = st.selectbox("Select your Diet Option", ["None", "Gluten Free", "Pescetarian", "Vegan", "Vegetarian", "Ketogenic"])
+        diet = st.selectbox("Select your Diet Option (Optional)", ["None", "Gluten Free", "Pescetarian", "Vegan", "Vegetarian", "Ketogenic"])
         if diet != 'None':
             recipe_search(search, diet = diet.lower())
         
